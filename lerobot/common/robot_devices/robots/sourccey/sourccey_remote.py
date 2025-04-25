a4f50c4c279ce1631069c1843c9637cfe6c3a709
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-04-25 02:22:57 -0400

    remove pdb

--- lerobot/common/robot_devices/robots/sourccey/sourccey_remote.py
+++ lerobot/common/robot_devices/robots/sourccey/sourccey_remote.py
@@ -112,8 +112,6 @@ def run_sourccey_v1beta(robot_config):
 
         print("here 8")
 
-        import pdb; pdb.set_trace()
-
         # Calibrate the follower arm.
         # calibrate_follower_arm(motors_bus, robot_config.calibration_dir)
 
