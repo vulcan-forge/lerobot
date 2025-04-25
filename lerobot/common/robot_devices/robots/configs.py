2d040cf86eca0d60a827158e49c886f457eec8ba
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-04-25 16:08:10 -0400

    usbo shows up

--- lerobot/common/robot_devices/robots/configs.py
+++ lerobot/common/robot_devices/robots/configs.py
@@ -672,7 +672,7 @@ class SourcceyV1BetaRobotConfig(MobileManipulatorRobotConfig):
     follower_arms: dict[str, MotorsBusConfig] = field(
         default_factory=lambda: {
             "main": FeetechMotorsBusConfig(
-                port= '/dev/ttyAMA0', #/dev/ttyTHS2 #COM16
+                port= '/dev/ttyUSB0', #/dev/ttyTHS2 #COM16
                 motors={
                     # name: (index, model)
                     # "shoulder_pan": [1, "sts3215"],
