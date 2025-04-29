56077b918481ce23082f55ea4b3d921974d21eb1
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-04-28 22:58:47 -0400

    wrist camera update

--- lerobot/common/robot_devices/robots/configs.py
+++ lerobot/common/robot_devices/robots/configs.py
@@ -638,16 +638,15 @@ class SourcceyV1BetaRobotConfig(MobileManipulatorRobotConfig):
 
     cameras: dict[str, CameraConfig] = field(
         default_factory=lambda: {
-
-            # "wrist_right": OpenCVCameraConfig(
+            # "front_right": OpenCVCameraConfig(
             #     camera_index="/dev/video4", fps=30, width=640, height=480, rotation=None
             # ),
-            "front_right": OpenCVCameraConfig(
-                camera_index="/dev/video12", fps=30, width=640, height=480, rotation=None
-            ),
             # "front_left": OpenCVCameraConfig(
             #     camera_index="/dev/video0", fps=30, width=640, height=480, rotation=None
             # ),
+            "wrist_right": OpenCVCameraConfig(
+                camera_index="/dev/video12", fps=30, width=640, height=480, rotation=None
+            ),
             "wrist_left": OpenCVCameraConfig(
                 camera_index="/dev/video8", fps=30, width=640, height=480, rotation=None
             ),
