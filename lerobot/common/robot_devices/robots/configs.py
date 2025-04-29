b97fa5366e96766c50091469b4899d5bb2213adb
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-04-28 21:43:05 -0400

    wrist left and right

--- lerobot/common/robot_devices/robots/configs.py
+++ lerobot/common/robot_devices/robots/configs.py
@@ -644,12 +644,12 @@ class SourcceyV1BetaRobotConfig(MobileManipulatorRobotConfig):
             "front_left": OpenCVCameraConfig(
                 camera_index="/dev/video8", fps=30, width=640, height=480, rotation=None
             ),
-            # "wrist_right": OpenCVCameraConfig(
-            #     camera_index="/dev/video4", fps=30, width=640, height=480, rotation=None
-            # ),
-            # "wrist_left": OpenCVCameraConfig(
-            #     camera_index="/dev/video0", fps=30, width=640, height=480, rotation=None
-            # ),
+            "wrist_right": OpenCVCameraConfig(
+                camera_index="/dev/video4", fps=30, width=640, height=480, rotation=None
+            ),
+            "wrist_left": OpenCVCameraConfig(
+                camera_index="/dev/video0", fps=30, width=640, height=480, rotation=None
+            ),
         }
     )
 
