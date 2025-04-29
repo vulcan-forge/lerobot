8e2c26fe5d0e2b1c9831b79dd557daaf03bb7041
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-04-28 20:38:08 -0400

    video 4 check

--- lerobot/common/robot_devices/robots/configs.py
+++ lerobot/common/robot_devices/robots/configs.py
@@ -644,9 +644,9 @@ class SourcceyV1BetaRobotConfig(MobileManipulatorRobotConfig):
             "front_two": OpenCVCameraConfig(
                 camera_index="/dev/video8", fps=30, width=640, height=480, rotation=None
             ),
-            "wrist": OpenCVCameraConfig(
-                camera_index="/dev/video4", fps=30, width=640, height=480, rotation=180
-            ),
+            # "wrist": OpenCVCameraConfig(
+            #     camera_index="/dev/video4", fps=30, width=640, height=480, rotation=180
+            # ),
             "wrist_two": OpenCVCameraConfig(
                 camera_index="/dev/video0", fps=30, width=640, height=480, rotation=180
             ),
