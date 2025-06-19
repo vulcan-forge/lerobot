14b3d13be9a07457741591ca4ac1f5424f78a87a
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-06-19 10:14:32 -0400

    gripper and wrist calibration

--- lerobot/common/robots/sourccey/sourccey_v2beta/sourccey_v2beta.py
+++ lerobot/common/robots/sourccey/sourccey_v2beta/sourccey_v2beta.py
@@ -154,7 +154,7 @@ class SourcceyV2Beta(Robot):
         #homing_offsets.update(dict.fromkeys(self.base_motors, 0))
 
         full_turn_motor = [
-            motor for motor in motors if any(keyword in motor for keyword in ["wheel", "wrist"])
+            motor for motor in motors if any(keyword in motor for keyword in ["wheel"])
         ]
         unknown_range_motors = [motor for motor in motors if motor not in full_turn_motor]
 
@@ -169,9 +169,11 @@ class SourcceyV2Beta(Robot):
 
         self.calibration = {}
         for name, motor in self.bus.motors.items():
+            # Set drive mode = 1 for grippers
+            drive_mode = 1 if "gripper" in name else 0
             self.calibration[name] = MotorCalibration(
                 id=motor.id,
-                drive_mode=0,
+                drive_mode=drive_mode,
                 homing_offset=homing_offsets[name],
                 range_min=range_mins[name],
                 range_max=range_maxes[name],
