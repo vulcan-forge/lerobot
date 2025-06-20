326eb8bbd31b9f47e12fb63c88ae429cb782d6b2
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-06-19 21:48:45 -0400

    15 fps

--- lerobot/common/robots/sourccey/sourccey_v2beta/config_sourccey_v2beta.py
+++ lerobot/common/robots/sourccey/sourccey_v2beta/config_sourccey_v2beta.py
@@ -34,7 +34,7 @@ class SourcceyV2BetaConfig(RobotConfig):
 
     cameras: dict[str, CameraConfig] = field(
         default_factory=lambda: {
-            "front_left": OpenCVCameraConfig(index_or_path="/dev/video4", fps=30, width=640, height=480),
+            "front_left": OpenCVCameraConfig(index_or_path="/dev/video4", fps=15, width=640, height=480),
             "front_right": OpenCVCameraConfig(
                 index_or_path="/dev/video12", fps=15, width=640, height=480
             ),
