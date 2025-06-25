ed5d00f55cd35a2c382c69023cd64bed794c6c20
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-06-25 10:48:08 -0400

    remove prints

--- lerobot/common/robots/so100_robot/so100_robot_host.py
+++ lerobot/common/robots/so100_robot/so100_robot_host.py
@@ -73,11 +73,7 @@ def main():
             loop_start_time = time.time()
             try:
                 msg = host.zmq_cmd_socket.recv_string(zmq.NOBLOCK)
-                if msg:
-                    print(msg)
                 data = dict(json.loads(msg))
-                if data:
-                    print(data)
                 _action_sent = robot.send_action(data)
                 last_cmd_time = time.time()
                 watchdog_active = False
