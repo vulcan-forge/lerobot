e5dc9cfc54b0950f3d94745d802ce5a00cde260c
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-06-27 16:49:32 -0400

    10 motors

--- start/checks/check_motor_sync_read.py
+++ start/checks/check_motor_sync_read.py
@@ -100,8 +100,7 @@ def test_feetech_motor_sync_read():
         print("Strategy 2: Reading arms separately...")
         try:
             left_arm_motors = ["left_arm_shoulder_pan", "left_arm_shoulder_lift", "left_arm_elbow_flex", "left_arm_wrist_flex", "left_arm_wrist_roll", "left_arm_gripper"]
-            right_arm_motors = ["right_arm_shoulder_pan", "right_arm_shoulder_lift", "right_arm_elbow_flex"]
-                            #    "right_arm_wrist_flex", "right_arm_wrist_roll", "right_arm_gripper"]
+            right_arm_motors = ["right_arm_shoulder_pan", "right_arm_shoulder_lift", "right_arm_elbow_flex", "right_arm_wrist_flex",] # "right_arm_wrist_roll", "right_arm_gripper"]
 
             left_positions = bus.sync_read("Present_Position", left_arm_motors, normalize=False)
             time.sleep(0.1)
