99fffaf7b53cfd0a846556eb0cec0c48b253cb2c
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-07-18 17:22:20 -0400

    odd camera index

--- lerobot/common/robots/sourccey_v2beta/config_sourccey_v2beta.py
+++ lerobot/common/robots/sourccey_v2beta/config_sourccey_v2beta.py
@@ -24,16 +24,16 @@ from lerobot.common.constants import HF_LEROBOT_CONFIGURATION
 def sourccey_v2beta_cameras_config() -> dict[str, CameraConfig]:
     return {
         "front_left": OpenCVCameraConfig(
-            index_or_path="/dev/video2", fps=30, width=320, height=240
+            index_or_path="/dev/video3", fps=30, width=320, height=240
         ),
         "front_right": OpenCVCameraConfig(
-            index_or_path="/dev/video4", fps=30, width=320, height=240
+            index_or_path="/dev/video5", fps=30, width=320, height=240
         ),
         "wrist_left": OpenCVCameraConfig(
-            index_or_path="/dev/video0", fps=30, width=320, height=240
+            index_or_path="/dev/video1", fps=30, width=320, height=240
         ),
         "wrist_right": OpenCVCameraConfig(
-            index_or_path="/dev/video6", fps=30, width=320, height=240
+            index_or_path="/dev/video7", fps=30, width=320, height=240
         ),
     }
 
