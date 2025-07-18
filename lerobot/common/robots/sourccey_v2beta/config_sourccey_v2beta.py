30cac416b8ebcbffc485e6c9852898b192d96ebb
Myles Reda <myles.reda2@gmail.com>
2025-07-18 16:10:58 -0400

    changin left arm port usb

--- lerobot/common/robots/sourccey_v2beta/config_sourccey_v2beta.py
+++ lerobot/common/robots/sourccey_v2beta/config_sourccey_v2beta.py
@@ -40,7 +40,7 @@ def sourccey_v2beta_cameras_config() -> dict[str, CameraConfig]:
 @RobotConfig.register_subclass("sourccey_v2beta")
 @dataclass
 class SourcceyV2BetaConfig(RobotConfig):
-    left_arm_port: str = "/dev/ttyUSB0"
+    left_arm_port: str = "/dev/ttyACM1"
     right_arm_port: str = "/dev/ttyACM0"
 
     disable_torque_on_disconnect: bool = True
