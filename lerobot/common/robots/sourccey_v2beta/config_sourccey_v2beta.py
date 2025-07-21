6df4b5bd1b775c10c5078b308575f4748798b37d
Myles Reda <myles.reda2@gmail.com>
2025-07-21 12:18:11 -0400

    camera fps and resolution change

--- lerobot/common/robots/sourccey_v2beta/config_sourccey_v2beta.py
+++ lerobot/common/robots/sourccey_v2beta/config_sourccey_v2beta.py
@@ -24,16 +24,16 @@ from lerobot.common.constants import HF_LEROBOT_CONFIGURATION
 def sourccey_v2beta_cameras_config() -> dict[str, CameraConfig]:
     config = {
         "front_left": OpenCVCameraConfig(
-            index_or_path="/dev/video0", fps=5, width=640, height=480
+            index_or_path="/dev/video0", fps=30, width=320, height=240
         ),
         "front_right": OpenCVCameraConfig(
-            index_or_path="/dev/video2", fps=5, width=640, height=480
+            index_or_path="/dev/video2", fps=30, width=320, height=240
         ),
         "wrist_left": OpenCVCameraConfig(
-            index_or_path="/dev/video4", fps=5, width=640, height=480
+            index_or_path="/dev/video4", fps=30, width=320, height=240
         ),
         "wrist_right": OpenCVCameraConfig(
-            index_or_path="/dev/video6", fps=5, width=640, height=480
+            index_or_path="/dev/video6", fps=30, width=320, height=240
         ),
     }
     print("Loaded camera config: ", config)
