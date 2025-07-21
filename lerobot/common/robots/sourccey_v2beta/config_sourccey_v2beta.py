2b541469592a944a8fca1319ef629fe774204378
Myles Reda <myles.reda2@gmail.com>
2025-07-21 12:01:32 -0400

    arm port fix

--- lerobot/common/robots/sourccey_v2beta/config_sourccey_v2beta.py
+++ lerobot/common/robots/sourccey_v2beta/config_sourccey_v2beta.py
@@ -42,8 +42,8 @@ def sourccey_v2beta_cameras_config() -> dict[str, CameraConfig]:
 @RobotConfig.register_subclass("sourccey_v2beta")
 @dataclass
 class SourcceyV2BetaConfig(RobotConfig):
-    left_arm_port: str = "/dev/ttyACM0"
-    right_arm_port: str = "/dev/ttyACM1"
+    left_arm_port: str = "/dev/ttyACM1"
+    right_arm_port: str = "/dev/ttyACM0"
 
     disable_torque_on_disconnect: bool = True
 
