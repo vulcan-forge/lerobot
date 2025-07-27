c48ef88fbf698a184f72e3bd26a8d7d2e76031b9
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-07-27 19:31:13 -0400

    reduced height

--- src/lerobot/robots/sourccey/sourccey_v3beta/sourccey_v3beta/config_sourccey_v3beta.py
+++ src/lerobot/robots/sourccey/sourccey_v3beta/sourccey_v3beta/config_sourccey_v3beta.py
@@ -24,16 +24,16 @@ from lerobot.constants import HF_LEROBOT_CONFIGURATION
 def sourccey_v3beta_cameras_config() -> dict[str, CameraConfig]:
     config = {
         "front_left": OpenCVCameraConfig(
-            index_or_path="/dev/video0", fps=30, width=320, height=240
+            index_or_path="/dev/video0", fps=30, width=640, height=360
         ),
         "front_right": OpenCVCameraConfig(
-            index_or_path="/dev/video2", fps=30, width=320, height=240
+            index_or_path="/dev/video2", fps=30, width=640, height=360
         ),
         "wrist_left": OpenCVCameraConfig(
-            index_or_path="/dev/video4", fps=30, width=320, height=240
+            index_or_path="/dev/video4", fps=30, width=640, height=360
         ),
         "wrist_right": OpenCVCameraConfig(
-            index_or_path="/dev/video6", fps=30, width=320, height=240
+            index_or_path="/dev/video6", fps=30, width=640, height=360
         ),
     }
     return config
